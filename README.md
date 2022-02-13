c50bbf4d9e3a91a492053fecc3f3735ebf9732e3
Tonya Oliver <tonya.anne.oliver@gmail.com>
2022-02-13 16:55:07 -0500

    Adding images, updating css, added events in js, updating html

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Oliver_Tonya_MT-FIP
+# Midterm Progress
+
+This is where I'm at for Midterm. Although I'm not as far as I'd like to be, I am very proud of my work so far. I am working alone on this project so balancing everything has been a brand new experience.
+
+# Usage
+To use this website:
+
+1. Clone repo to your remote device
+
+2. DO NOT push your work to the main branch! Create a new branch name that makes sense, make changes to improve the site, push your BRANCH back to the repo.
+
+3. Wait for confirmation of merge!
+
+Thank you so much for the interest in my project!
+
+# Credits
+Tonya Oliver, First year Interactive Media Design student.
+
+# License
+Copyright 2022
--- js/main.js
+++ js/main.js
@@ -1,24 +1,21 @@
 (() => {
-  //this is js comment
+
   console.log('this is some text');
 
-  //go into the document and make a reference to the element that you want to work with
-  let theHeading = document.querySelector(".main-heading"),
-      theIcons = document.querySelectorAll(".icon");
+  let theHeading = document.querySelector("#mainHeader"),
+      theLogo = document.querySelector("#logo"),
+      HeroImg = document.querySelector("#Hero"),
+      
 
-//create a function that runs when theHeader is clicked
   function logElement() {
     console.log('clicked on the element:', this.id);
   }
 
-//listen for a oser event and something
-//the do something is the function we run when the element is clicked
   theHeading.addEventListener("click", logElement);
-  /*this is a block comment
-  that can run on multiple lines*/
 
-//loop through all of the icons and add event handling
-//process each icon at a time and add behaviour
-  theIcons.forEach(icon => icon.addEventListener('click', logElement));
+  theLogo.addEventListener('click', logElement);
+
+  HeroImg.addEventListener('click', logElement);
+
 
 })();
